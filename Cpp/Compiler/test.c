8fbf0454561bb885adfd21c546553b78baacd707
Abhay <abhay9874patil@gmail.com>
2023-02-25 19:09:07 -0800

    Initial

---
+++ Cpp/Compiler/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+  int x = 20;
+  double y = 100;
+  
+  printf("Hello World!\n");
+  
+  double logy = log10(y);
+  printf("logy = %f\n", logy);
+  
+  return 0;
+}
